d5e978c2ceb9d3ba8992396f6ccab850c9375cf4
Firas <448445223@qq.com>
2020-03-19 07:44:49 +0800

    Adding tests for StringListToDbProcessor

---
+++ src/test/java/tech/firas/framework/fileimport/processor/db/test/StringListToDbProcessorTests.java
@@ -0,0 +1,24 @@
+package tech.firas.framework.fileimport.processor.db.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tech.firas.framework.fileimport.test.AbstractTests;
+
+public class StringListToDbProcessorTests extends AbstractTests {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testWithDataSource() {
+
+    }
+
+    @Test
+    public void testWithJdbcTemplate() {
+
+    }
+}
